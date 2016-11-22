c1df10ec491271328a1c71a39ab9ac96b8ea62fb
private <user@example.com>
2016-11-22 01:59:39 +0000

    Add a form and the two routes for adding the data and sending it.

--- Treasuregram/main_app/urls.py
+++ Treasuregram/main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^([0-9]+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.add_treasure, name='add_treasure'),
+    url(r'^add/post_url/$', views.post_treasure, name='post_treasure'),
 ]
--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
 from .models import Treasure
+from .forms  import TreasureForm
 
 def index(request):
     treasures = Treasure.objects.all()
@@ -8,3 +10,20 @@ def index(request):
 def detail(request, treasure_id):
     treasure = Treasure.objects.get(id=treasure_id)
     return render(request, 'detail.html', {'treasure': treasure})
+
+def add_treasure(request):
+    form = TreasureForm()
+    return render(request, 'add.html', {'form':form})
+
+def post_treasure(request):
+    form = TreasureForm(request.POST)
+    if form.is_valid():
+        teasure = Treasure(
+            name     = form.cleaned_data['name'],
+            value    = form.cleaned_data['value'],
+            material = form.cleaned_data['material'],
+            location = form.cleaned_data['location'],
+            img_url  = form.cleaned_data['img_url']
+        )
+        treasure.save()
+    return HttpResponseRedirect('/')
